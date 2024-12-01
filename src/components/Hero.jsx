22ad6ae575cd7322532a247106d130b6f8bd3e30
Saffat Aziz <saziz028@uottawa.ca>
2024-11-30 20:00:17 -0800

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,7 +7,6 @@ import { ScrollParallax } from 'react-just-parallax';
 import { useRef } from 'react';
 import Generating from './Generating';
 import Notification from './Notification';
-// import CompanyLogos from './CompanyLogos';
 
 const Hero = () => {
 	const parallaxRef = useRef(null);
