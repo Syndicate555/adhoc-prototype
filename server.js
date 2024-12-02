0136db6057ca1e068a1d74833d1dd091cbee3660
Saffat Aziz <saziz028@uottawa.ca>
2024-12-02 14:09:08 -0800

    Update server.js

--- server.js
+++ server.js
@@ -4,19 +4,15 @@ import { fileURLToPath } from 'url';
 
 const app = express();
 
-// Define __dirname for ES Modules
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// Serve static files from the dist directory
 app.use(express.static(path.join(__dirname, 'dist')));
 
-// Handle all GET requests by serving the index.html file
 app.get('*', (req, res) => {
 	res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
-// Start the server on the port provided by Heroku
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
 	console.log(`Server is running on port ${PORT}`);
